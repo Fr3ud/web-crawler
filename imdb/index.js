ea793212d9b3024d5f5aab5c1348052eb92a88e4
Fr3ud <oo.pavelko@gmail.com>
2021-01-16 22:35:08 +0200

    feat: add simple IMDB scraper

---
+++ imdb/index.js
@@ -0,0 +1,15 @@
+const request = require('request-promise');
+const cheerio = require('cheerio');
+
+const url = 'https://www.imdb.com/title/tt10048342/?ref_=hm_fanfav_tt_3_pd_fp1';
+
+(async () => {
+
+    const response = await request(url);
+
+    let $ = cheerio.load(response);
+    let title = $('div[class="title_wrapper"] > h1').text();
+
+    console.log(title);
+
+})();
