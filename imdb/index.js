b667b035f2bfce16715afebb1c3b601a7f22b6ac
Fr3ud <oo.pavelko@gmail.com>
2021-01-22 19:45:53 +0200

    feat: add rating

--- imdb/index.js
+++ imdb/index.js
@@ -9,7 +9,8 @@ const url = 'https://www.imdb.com/title/tt10048342/?ref_=hm_fanfav_tt_3_pd_fp1';
 
     let $ = cheerio.load(response);
     let title = $('div[class="title_wrapper"] > h1').text();
+    let rating = $('span[itemprop="ratingValue"]').text();
 
-    console.log(title);
+    console.log(title, rating);
 
 })();
